055c97e24d19101f15de8489e97f46d038e91de7
Иван Иванов <ivan123@gmail.com>
2025-04-23 23:01:52 +0700

    Removed .idea, okolegius.zip, and config.py from repository

--- config.py
+++
@@ -1,8 +0,0 @@
-# Токен бота
-BOT_TOKEN = "7427355085:AAHkc_FoasqupEsMCIzYebuBKJQBYkIY6sA"
-
-# Конфигурация базы данных
-DATABASE_TYPE = "sqlite"
-DATABASE_NAME = "university_bot.db"
-ADMIN_ID = 6031684537  # ID администратора
-MEDIA_PATH = "media"   # Путь к папке для медиафайлов
